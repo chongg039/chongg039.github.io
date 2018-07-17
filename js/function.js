91948c23dc55f452bee34995416ee218a047151c
chongg039 <chongg039@gmail.com>
2018-07-17 11:54:12 +0800

    poisson process

---
+++ js/function.js
@@ -0,0 +1,6 @@
+(function (){
+    var pres = document.querySelectorAll("pre");
+    for (i = 0; i < pres.length; i++) {
+        pres[i].setAttribute("class", "prettyprint linenums");
+    }
+})();
